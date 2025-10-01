3b4957270eeca69251afb71b119c7dc6875e3db1
Yovany Morales <83714102+yovany-dev@users.noreply.github.com>
2025-10-01 17:30:48 +0000

    chore(section-1): add comments

--- SECCIÓN I/name_normalization.js
+++ SECCIÓN I/name_normalization.js
@@ -1,3 +1,5 @@
+// Función para normalizar nombres
+
 function normalizeName(name) {
   if (!name) return '';
 
@@ -6,10 +8,13 @@ function normalizeName(name) {
     s.charAt(0).toLocaleUpperCase('es-ES') +
     s.slice(1).toLocaleLowerCase('es-ES');
 
+  // Tim quitamos espacio inicio/fin
+  // Split divide por 1 o más espacios y elimina espacios extra
+  // Map aplica capitalización a cada palabra
+  // Join une con un espacio normal
   return name.trim().split(/\s+/).map(capitalize).join(' ');
 }
 
 // Ejemplos:
 console.log(normalizeName(' juan carLos pérez lopez ')); // "Juan Carlos Pérez Lopez"
-
 console.log(normalizeName('   MARÍA   FERNANDA   ')); // "María Fernanda"
